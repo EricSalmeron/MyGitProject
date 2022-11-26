539e52aac351d84c84e64bee6c008ab6db98cccb
EricSalmeron <69407681+EricSalmeron@users.noreply.github.com>
2022-11-26 17:23:12 -0500

    dfef

---
+++ client/script.js
@@ -0,0 +1,10 @@
+
+function dateBut() {
+    document.getElementById("demo").innerHTML = Date();
+
+}
+
+document.querySelector('.next')
+    .addEventListener('click', () => {
+        console.log('clicked next');
+    })
---
+++ server/apiRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import path from 'path'
+
+import foodServiceRoutes from './foodServiceRoutes.js.js.js';
+import sqlDemoRoutes from './sqlDemoRoutes.js.js.js'; 
+
+const router = express.Router();
